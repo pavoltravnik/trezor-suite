181691f79cf07202c2a6a34b191d2fc269bd35d2
Szymon Lesisz <sz.lesisz@gmail.com>
2021-12-03 17:03:39 +0100

    refactor(suite): replace react-redux binding with hooks

--- packages/suite/src/components/suite/modals/PassphraseOnDevice/index.tsx
+++ packages/suite/src/components/suite/modals/PassphraseOnDevice/index.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { ConfirmOnDevice } from '@trezor/components';
-import { Modal, ModalProps } from '@suite-components';
+import { Modal } from '@suite-components';
 import { Translation } from '@suite-components/Translation';
+import { useActions } from '@suite-hooks';
 import * as discoveryActions from '@wallet-actions/discoveryActions';
-import { Dispatch, TrezorDevice } from '@suite-types';
-
 import DeviceConfirmImage from '@suite-components/images/DeviceConfirmImage';
+import type { TrezorDevice } from '@suite-types';
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    getDiscoveryAuthConfirmationStatus: () =>
-        dispatch(discoveryActions.getDiscoveryAuthConfirmationStatus()),
-});
-
-interface OwnProps extends ModalProps {
+interface Props {
     device: TrezorDevice;
 }
-
-type Props = OwnProps & ReturnType<typeof mapDispatchToProps>;
 
 /**
  * Modal used with model T with legacy firmware as result of 'ButtonRequest_PassphraseType' where passphrase source is requested on device
  * @param {Props}
  */
-const PassphraseOnDevice = ({ device, getDiscoveryAuthConfirmationStatus, ...rest }: Props) => {
+const PassphraseOnDevice = ({ device }: Props) => {
+    const { getDiscoveryAuthConfirmationStatus } = useActions({
+        getDiscoveryAuthConfirmationStatus: discoveryActions.getDiscoveryAuthConfirmationStatus,
+    });
     const authConfirmation = getDiscoveryAuthConfirmationStatus() || device.authConfirm;
 
     if (authConfirmation) {
@@ -37,7 +32,6 @@ const PassphraseOnDevice = ({ device, getDiscoveryAuthConfirmationStatus, ...res
                     />
                 }
                 description={<Translation id="TR_THIS_HIDDEN_WALLET_IS_EMPTY_SOURCE" />}
-                {...rest}
             >
                 <DeviceConfirmImage device={device} />
                 {/* TODO: similar text is in Passphrase modal */}
@@ -62,11 +56,10 @@ const PassphraseOnDevice = ({ device, getDiscoveryAuthConfirmationStatus, ...res
             }
             description={<Translation id="TR_PASSPHRASE_CASE_SENSITIVE" />}
             data-test="@modal/enter-passphrase-on-device"
-            {...rest}
         >
             <DeviceConfirmImage device={device} />
         </Modal>
     );
 };
 
-export default connect(null, mapDispatchToProps)(PassphraseOnDevice);
+export default PassphraseOnDevice;
